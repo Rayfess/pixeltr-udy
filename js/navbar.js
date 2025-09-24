5c9573433e96f51e7013db5ae1b726f0d49d6cb6
Rayfess <srayfes@gmail.com>
2025-09-24 19:25:54 +0700

    dev: navbar responsive 1.0.0

---
+++ js/navbar.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  // Efek scroll pada navbar
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 100) {
+      $(".navbar").addClass("scrolled");
+      $("#navbar-title").addClass("text-success");
+    } else {
+      $(".navbar").removeClass("scrolled");
+      $("#navbar-title").removeClass("text-success");
+    }
+  });
+
+  // Menutup sidebar saat mengklik item dropdown
+  $(".dropdown-item").on("click", function () {
+    $(".offcanvas").offcanvas("hide");
+  });
+
+  // Mencegah dropdown di sidebar menutup saat diklik
+  $(document).on("click", ".offcanvas .dropdown-menu", function (e) {
+    e.stopPropagation();
+  });
+
+  // Sembunyikan navbar ketika sidebar terbuka
+  $("#offcanvasNavbar").on("show.bs.offcanvas", function () {
+    $("body").addClass("sidebar-open");
+  });
+
+  $("#offcanvasNavbar").on("hide.bs.offcanvas", function () {
+    $("body").removeClass("sidebar-open");
+  });
+});
