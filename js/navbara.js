03e7a41479f1ef458b540cb4c59bfca7e851857e
Rayfess <srayfes@gmail.com>
2025-09-30 23:53:34 +0700

    dev: gabung 1.0.13 ,, refactor navbar codes

---
+++ js/navbara.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  // Efek scroll pada navbar
+  $(window).scroll(function () {
+    $(".navbar").toggleClass("scrolled", $(window).scrollTop() > 0);
+  });
+
+  // Menutup sidebar saat mengklik item dropdown
+  $(".dropdown-item").on("click", function () {
+    $(".offcanvas").offcanvas("hide");
+  });
+
+  // Mencegah dropdown di sidebar menutup saat diklik
+  $(document).on("click", ".offcanvas .dropdown-menu", function (e) {
+    e.stopPropagation();
+  });
+
+  // Sembunyikan navbar ketika sidebar terbuka
+  $("#offcanvasNavbar").on("show.bs.offcanvas", function () {
+    $("body").addClass("sidebar-open");
+  });
+
+  $("#offcanvasNavbar").on("hide.bs.offcanvas", function () {
+    $("body").removeClass("sidebar-open");
+  });
+});
